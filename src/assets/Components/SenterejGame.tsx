cec3eeac755af58de03b622ab98a9c2acc64f801
Abemelek97 <abemelek.elias@gmail.com>
2025-02-12 02:39:15 -0800

    Wta beka message

--- src/assets/Components/SenterejGame.tsx
+++ src/assets/Components/SenterejGame.tsx
@@ -1,12 +1,12 @@
 import SenterejGameWithHighlight from './SenterejGameWithHighlight';
 
 const SenterejGame: React.FC = () => {
-
+ 
   return(
         <div>
-            <h1>Senterej Game</h1>
-          <SenterejGameWithHighlight/>
-        </div>
+            <h1>ሰንጠረዥ</h1>
+          <SenterejGameWithHighlight/>          
+          </div>
    )
 };
 export default SenterejGame;
